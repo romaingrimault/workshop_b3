8259cfbdc0baeacdc557aab376adabd46c3ff4fc
SEVELudivine <73937165+SEVELudivine@users.noreply.github.com>
2020-11-05 13:31:44 +0100

    qcm

---
+++ vue/v_finQCM.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <script language="JavaScript">
+        window.history.forward();
+        </script>
+        <script type="text/javascript"></script>
+        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
+        <script type="text/javascript" src="ajax/script.js"></script>
+    </head>
+    <body>
+        <br/><br/>
+        <H2>Félicitation, vous avez obtenu <?php echo $totalpts['pt']; ?> point(s) pour ce QCM.</H2>
+
+       
+    </body>
+
+</html>
+
