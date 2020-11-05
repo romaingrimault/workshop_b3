3a2e7da5c9867306479a60036523e8e5e1ee4e45
SEVELudivine <73937165+SEVELudivine@users.noreply.github.com>
2020-11-05 13:32:29 +0100

    qcm

--- controleur/c_QCM.php
+++ controleur/c_QCM.php
@@ -1,3 +1,4 @@
+<script type="text/javascript" src="ajax/script.js"></script>
 <?php
 if ( $_SERVER["SCRIPT_FILENAME"] == __FILE__ ){
     $racine="..";
@@ -9,12 +10,41 @@ switch($action)
 {
 	case 'qcm':
 	{ 
-		include_once "$racine/modele/bd.QCM.inc.php";
-
+		
+		$nbQ = getNombresQuestionsQCM();
+		//$listeIdQ = getListeIdQuestions();
 		$questions = getQuestionsQCM();
 		$reponses = getReponsesQCM();
-		
+
 		include "$racine/vue/v_QCM.php";
+		?> <script>
+			var init = function () {
+				var div1 = document.getElementById(1);
+				div1.style.display = "block";
+			};
+			// on exécute la fonction
+			init();
+		</script> <?php
+		break;
+	}
+	case 'confirmqcm':
+	{ 
+		$questions = getQuestionsQCM();	
+		foreach($questions as $uneQuestion) 
+        { 
+        	$iduneQuestion = $uneQuestion['id'];
+			$idLaReponse = $_REQUEST[$iduneQuestion];
+			reponseUtilisateur($iduneQuestion, $idLaReponse, '3'); //Envoyer les données dans la bdd
+			//Remplacer 3 par l'id de l'utilisateur
+
+		}
+		
+		$totalpts = totalPtsQCM('3');
+		include "$racine/vue/v_finQCM.php";
+		//Remplacer 3 par l'id de l'utilisateur
+		//include "$racine/vue/v_QCM.php";
+
+
 		break;
 	}
 }
